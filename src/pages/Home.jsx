8cd44ed31607689c88e1138f79a6c2a8a012f556
Matthew Gilligan <mcgilligan97@gmail.com>
2022-09-05 17:46:38 +0100

    Add active image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,67 @@
-import { createEffect } from 'solid-js';
+import { createEffect, createSignal, For } from 'solid-js';
 import { MetaProvider } from 'solid-meta';
 import { Mousewheel, Keyboard } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/solid';
 
+import Autumn from './../assets/thumbnails/autumn.jpg';
+import Barber from './../assets/thumbnails/barber.jpg';
+import Canary from './../assets/thumbnails/canary.jpg';
+
 import 'swiper/css';
 
 export default function HomePage() {
+  const [articles, setArticles] = createSignal([
+    {
+      title: 'When Socialists Built our Homes, Joe Mathieson',
+      slug: 'when-socialists-built-our-homes',
+      img: Autumn,
+    },
+    {
+      title:
+        "The Far Right's Obsession With Modern Architecture, Joe Mathieson",
+      slug: 'the-far-rights-obsession-with-modern-architecture',
+      img: Canary,
+    },
+    {
+      title:
+        'A Small Corner of Enfield shows how good modern social housing can be, Joe Mathieson',
+      slug: 'a-small-corner-of-enfield-shows',
+      img: Barber,
+    },
+    {
+      title: 'When Socialists Built our Homes, Joe Mathieson',
+      slug: 'when-socialists-built-our-homes',
+      img: Autumn,
+    },
+    {
+      title:
+        "The Far Right's Obsession With Modern Architecture, Joe Mathieson",
+      slug: 'the-far-rights-obsession-with-modern-architecture',
+      img: Canary,
+    },
+    {
+      title:
+        'A Small Corner of Enfield shows how good modern social housing can be, Joe Mathieson',
+      slug: 'a-small-corner-of-enfield-shows',
+      img: Barber,
+    },
+    {
+      title: 'When Socialists Built our Homes, Joe Mathieson',
+      slug: 'when-socialists-built-our-homes',
+      img: Autumn,
+    },
+    {
+      title:
+        "The Far Right's Obsession With Modern Architecture, Joe Mathieson",
+      slug: 'the-far-rights-obsession-with-modern-architecture',
+      img: Canary,
+    },
+  ]);
+
+  const [activeImg, setActiveImg] = createSignal(articles()[0].img);
+
+  console.log(activeImg());
+
   const removeEffect = () => {
     Array.from(document.querySelectorAll('.swiper-slide')).forEach(function (
       el
@@ -18,20 +74,30 @@ export default function HomePage() {
     });
   };
 
-  const hello = () => {
+  const transitionUpdate = () => {
     removeEffect();
-    var previousSlide = document.querySelector(
-      '.swiper-slide-active'
-    ).previousSibling;
-    var thirdSlide = document.querySelector('.swiper-slide-next').nextSibling;
-    var fourthSlide = thirdSlide.nextSibling;
+    const activeSlide = document.querySelector('.swiper-slide-active');
+    const previousSlide = activeSlide.previousSibling;
+    const thirdSlide = document.querySelector('.swiper-slide-next').nextSibling;
+    const fourthSlide = thirdSlide.nextSibling;
+
     previousSlide.classList.add('swiper-slide-previous');
     thirdSlide.classList.add('swiper-slide-third');
     fourthSlide.classList.add('swiper-slide-fourth');
+
+    const activeSlug = activeSlide.getAttribute('data-slug');
+    console.log(activeSlug);
+
+    articles().forEach((article, index) => {
+      if (article.slug === activeSlug) {
+        setActiveImg(article.img);
+      }
+    });
   };
 
   return (
     <MetaProvider>
+      <img src={activeImg()} alt='Active Image' class='active-img' />
       <Swiper
         modules={[Mousewheel, Keyboard]}
         direction='vertical'
@@ -41,25 +107,15 @@ export default function HomePage() {
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
         slidesPerView={3}
-        onTransitionStart={hello}
+        onTransitionStart={transitionUpdate}
       >
-        <SwiperSlide>
-          <p>When Socialists Built our Homes, Joe Mathieson</p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <p>
-            The Far Right's Obsession With Modern Architecture, Joe Mathieson
-          </p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <p>
-            A Small Corner of Enfield shows how good modern social housing can
-            be, Joe Mathieson
-          </p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <p>Streamline in the suburbs, Joe Mathieson</p>
-        </SwiperSlide>
+        <For each={articles()}>
+          {(article, i) => (
+            <SwiperSlide data-slug={article.slug}>
+              <p>{article.title}</p>
+            </SwiperSlide>
+          )}
+        </For>
       </Swiper>
     </MetaProvider>
   );
